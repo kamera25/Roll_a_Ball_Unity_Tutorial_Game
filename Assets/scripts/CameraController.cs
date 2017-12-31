a84bd2c8b1b1f6d194598ecb7671b3d8e8afe7e6
Luca Quatela <luca.quatela@openmindonline.it>
2017-12-31 19:35:17 +0100

    add script to make the camera follow the player around

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start() {
+		offset = transform.position - player.transform.position;
+	}
+
+	// late update gets executed, every frame, after all items has been processed in update
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+
+}
