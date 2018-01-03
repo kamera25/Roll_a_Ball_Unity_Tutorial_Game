3c968b8ae817daef52ac22edca87c4d9dec1ac66
Luca Quatela <luca.quatela@openmindonline.it>
2018-01-03 19:35:44 +0100

    check for collision between the player and the collectible cubes and remove the cubes when the collision happen

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
 
 	// create a rigidBody to which we will apply the forces
 	private Rigidbody rb;
+	private int count;
 	public float speed;
+	// object of type text, it's a reference to the text UI element
+	public Text countText;
 
 	void Start() {
+		count = 0;
 		// get the rigidbody of the component to which the script is attached
 		rb = GetComponent<Rigidbody>();
+		updateText ();
 	}
 
 	void FixedUpdate() {
@@ -23,6 +29,18 @@ public class playerController : MonoBehaviour {
 
 		// apply the forces to the RigidBody
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			updateText ();
+		}
+	}
+		
+	void updateText() {
+		countText.text = "Count = " + count.ToString ();
 	}
 
 }
